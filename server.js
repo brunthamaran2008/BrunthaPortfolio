ffb06c2684e3b42a573fa6b2f1042ef3a2e7b6c1
brunthamaran2008 <brunthamaran380@gmail.com>
2026-09-15 18:18:26 +0530

    Change MySQL connection password to empty string
    Updated MySQL connection password to an empty string.

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.json());
 const db = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: "Bru@2008",
+    password: "",
     database: "portfolio_db"
 });
 
